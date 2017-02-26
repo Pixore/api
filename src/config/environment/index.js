8e342411c0b242a7a59787fbe026702be754ef0a
albizures <albizures3601@gmail.com>
2017-02-26 10:58:45 -0600

    fix(twitter): remove callback url

--- src/config/environment/index.js
+++ src/config/environment/index.js
@@ -3,8 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const path = require('path')
 
 const config = {}
-config.MONGODB_URI = 'mongodb://mongodb:' + process.env.MONGODB_PORT_27017_TCP_PORT + '/pixore'
-config.PORT = Number(process.env.PORT) || 8000
+config.MONGODB_URI = 'mongodb://mongodb/pixore'
+config.PORT = Number(process.env.PORT) || 80
 config.isDev = process.env.NODE_ENV === 'development'
 config.isTest = process.env.NODE_ENV === 'test'
 config.ROOT_PATH = path.join(__dirname, '..', '..', '..', '..')
@@ -20,7 +20,7 @@ config.auth = {
   twitter: {
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: `http://127.0.0.1:${config.PORT}/api/auth/twitter/callback`,
+    // callbackURL: `http://127.0.0.1:${config.PORT}/api/auth/twitter/callback`,
     includeEmail: true
   }
 }
