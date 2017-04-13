fa60ed8b8968cf3d22f2d6bda35ffb5ba403c2b1
Jose Albizures <albizures3601@gmail.com>
2017-04-12 21:54:43 -0600

    test(Users): Fix tests

--- src/routes/users/users.spec.js
+++ src/routes/users/users.spec.js
@@ -6,9 +6,8 @@ const { User } = require('../../models')
 
 describe('Users', () => {
   let id
-  before(function () {
-    return User.remove({})
-  })
+  before(() => User.remove({}))
+  after(() => User.remove({}))
   it('GET / empty array', done => {
     request(app)
       .get('/api/users/')
@@ -33,79 +32,37 @@ describe('Users', () => {
         done()
       })
   })
-  it('POST / ok', done => {
-    request(app)
-      .post('/api/users')
-      .send({
-        username: 'testname',
-        displayName: 'Test Name',
-        email: 'email@test.com'
-      })
-      .set('Connection', 'keep-alive')
-      .expect('Content-Type', /json/)
-      .expect(201)
-      .end(function (err, res) {
-        id = res.body._id
-        expect(res.body).to.have.all.keys('__v', 'username', 'displayName', 'email', '_id', 'profileImage')
-        expect(res.body.username).to.equal('testname')
-        expect(res.body.displayName).to.equal('Test Name')
-        expect(res.body.email).to.equal('email@test.com')
-        expect(res.body.profileImage).to.be.a('string')
-        expect(err).to.be.null
-        done()
-      })
-  })
-  it('GET / one element', done => {
-    request(app)
-      .get('/api/users/')
-      .set('Connection', 'keep-alive')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body.length).to.equal(1)
-        expect(err).to.be.null
-        done()
-      })
-  })
   it('GET /:id Found', done => {
-    request(app)
-      .get('/api/users/' + id)
-      .set('Connection', 'keep-alive')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.body).to.have.any.keys('username', 'displayName', '_id', 'profileImage')
-        expect(err).to.be.null
-        done()
-      })
+    User.create({
+      username: 'testName',
+      email: 'email@test.com',
+      displayName: 'Test Name',
+      twitterID: 1234,
+      profileImage: '/assets/images/profile.png'
+    }).then(user => {
+      id = user._id
+      request(app)
+        .get('/api/users/' + id)
+        .set('Connection', 'keep-alive')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+          console.log(err, res.body)
+          expect(res.body).to.have.any.keys('username', 'displayName', '_id', 'profileImage')
+          expect(err).to.be.null
+          done()
+        })
+    })
   })
-
   it('GET /:id invalid id', done => {
     request(app)
-      .get('/api/users/an-invalid-name')
+      .get('/api/users/an-invalid-id')
       .set('Connection', 'keep-alive')
       .expect('Content-Type', /json/)
       .expect(500)
       .end(function (err, res) {
         expect(err).to.be.null
         expect(res.body).to.have.any.keys('message', 'path', 'type', 'context')
-        done()
-      })
-  })
-  it('POST /:id invalid property', done => {
-    request(app)
-      .post('/api/users')
-      .send({
-        username: 'test-name',
-        displayName: 'Test Name',
-        email: 'non-email'
-      })
-      .set('Connection', 'keep-alive')
-      .expect('Content-Type', /json/)
-      .expect(500)
-      .end(function (err, res) {
-        expect(res.body).to.have.any.keys('message', 'path', 'type', 'context')
-        expect(err).to.be.null
         done()
       })
   })
