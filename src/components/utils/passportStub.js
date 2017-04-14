857b636a61dca9b504671197e88c2024c75b2c08
Jose Albizures <albizures3601@gmail.com>
2017-04-14 12:39:23 -0600

    fix(passport): fix passportStub

--- src/components/utils/passportStub.js
+++ src/components/utils/passportStub.js
@@ -1,79 +1,59 @@
 // BASE ON: https://github.com/gtramontina/passport-stub
 
-var done, passportStub
-
-done = function (user, req, next) {
+const done = function (user, req, next) {
   return next(null, user)
 }
 
-passportStub = (function (_this) {
-  return function (req, res, next) {
-    var passport
-    if (!_this.active) {
-      return next()
-    }
-    passport = {
-      deserializeUser: done,
-      serializeUser: done,
-      _userProperty: 'user',
-      _key: 'passport'
-    }
-    req.__defineGetter__('_passport', function () {
-      return {
-        instance: passport,
-        session: {
-          user: _this.user
-        }
-      }
-    })
-    req.__defineGetter__('user', function () {
-      return _this.user
-    })
-    req.__defineSetter__('user', function (val) {
-      _this.user = val
-    })
+const state = {}
+
+const passportStub = (req, res, next) => {
+  var passport
+  if (!state.active) {
     return next()
   }
-})(this)
-
-exports.use = function (app) {
-  this.app = app
-  return this.app.use(passportStub)
-}
-
-exports.install = function (app) {
-  this.app = app
-  return this.app._router.stack.unshift({
-    match: function () {
-      return true
-    },
-    path: '',
-    handle: passportStub,
-    handle_request: passportStub,
-    _id: 'passport.stub'
-  })
-}
-
-exports.uninstall = function () {
-  if (!this.app) {
-    return
+  passport = {
+    deserializeUser: done,
+    serializeUser: done,
+    _userProperty: 'user',
+    _key: 'passport'
   }
-  return this.app._router.stack.forEach(function (middleware, index, stack) {
-    if (middleware._id === 'passport.stub') {
-      return stack.splice(index, 1)
+  req.__defineGetter__('_passport', function () {
+    return {
+      instance: passport,
+      session: {
+        user: state.user
+      }
     }
   })
+  req.__defineGetter__('user', function () {
+    return state.user
+  })
+  req.__defineSetter__('user', function (val) {
+    state.user = val
+  })
+  return next()
 }
 
-exports.login = function (user) {
-  if (!this.app) {
-    throw new Error('Passport Stub not installed. Please run ' + passportStub.install(this.app) + ' first.')
+const use = app => {
+  state.app = app
+  return state.app.use(passportStub)
+}
+
+const login = user => {
+  if (!state.app) {
+    throw new Error('Passport Stub not installed.')
   }
-  this.active = true
-  this.user = user
+  state.active = true
+  state.user = user
 }
 
-exports.logout = function () {
-  this.active = false
-  this.user = undefined
+const logout = () => {
+  state.active = false
+  state.user = undefined
+}
+
+module.exports = {
+  login,
+  logout,
+  use
 }
